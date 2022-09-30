90db4ffec986b5c37f1dacc1ca1af282051a5c25
Vedant Koditkar <vedant.koditkar@outlook.com>
2022-10-01 00:41:46 +0530

    Make PasswordOptions Clone-able

--- Password.Core/PasswordOptions.cs
+++ Password.Core/PasswordOptions.cs
@@ -1,10 +1,22 @@
 ﻿namespace Password.Core;
 
-public class PasswordOptions
+public class PasswordOptions : ICloneable
 {
     public int Length { get; set; }
     public bool IncludeLowerCaseLetters { get; set; }
     public bool IncludeUpperCaseLetters { get; set; }
     public bool IncludeDigits { get; set; }
     public bool IncludeSymbols { get; set; }
+
+    public object Clone()
+    {
+        return new PasswordOptions
+        {
+            Length = Length,
+            IncludeDigits = IncludeDigits,
+            IncludeSymbols = IncludeSymbols,
+            IncludeLowerCaseLetters = IncludeLowerCaseLetters,
+            IncludeUpperCaseLetters = IncludeUpperCaseLetters
+        };
+    }
 }
