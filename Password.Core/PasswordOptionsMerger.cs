e698e2b38ca8899d27fe7f84c5f2e53ed6eefb69
Vedant Koditkar <vedant.koditkar@outlook.com>
2022-10-01 00:42:17 +0530

    Add PasswordOptionsMerger and default password options

---
+++ Password.Core/PasswordOptionsMerger.cs
@@ -0,0 +1,43 @@
+﻿namespace Password.Core;
+
+internal static class PasswordOptionsMerger
+{
+    private static readonly PasswordOptions DefaultPasswordOptions = new()
+    {
+        Length = 15,
+        IncludeLowerCaseLetters = true,
+        IncludeDigits = true,
+        IncludeSymbols = true,
+        IncludeUpperCaseLetters = true
+    };
+
+    internal static PasswordOptions Merge(PasswordOptions options)
+    {
+        var mergedOptions = (PasswordOptions) DefaultPasswordOptions.Clone();
+
+        if (options.Length > 0)
+        {
+            mergedOptions.Length = options.Length;
+        }
+
+        if (options.IncludeSymbols
+            || options.IncludeDigits
+            || options.IncludeUpperCaseLetters
+            || options.IncludeLowerCaseLetters)
+        {
+            mergedOptions.IncludeLowerCaseLetters = options.IncludeLowerCaseLetters;
+            mergedOptions.IncludeUpperCaseLetters = options.IncludeUpperCaseLetters;
+            mergedOptions.IncludeDigits = options.IncludeDigits;
+            mergedOptions.IncludeSymbols = options.IncludeSymbols;
+        }
+        else
+        {
+            mergedOptions.IncludeLowerCaseLetters = true;
+            mergedOptions.IncludeUpperCaseLetters = DefaultPasswordOptions.IncludeUpperCaseLetters;
+            mergedOptions.IncludeDigits = DefaultPasswordOptions.IncludeDigits;
+            mergedOptions.IncludeSymbols = DefaultPasswordOptions.IncludeSymbols;
+        }
+
+        return mergedOptions;
+    }
+}
---
+++ Password.UnitTests/PasswordOptionsMergerTests.cs
@@ -0,0 +1,78 @@
+﻿using Password.Core;
+using Xunit;
+
+namespace Password.UnitTests;
+
+public class PasswordOptionsMergerTests
+{
+    [Fact]
+    public void Should_Return_Default_Options_If_No_Override_Configuration_Given()
+    {
+        // Arrange
+        var options = new PasswordOptions();
+
+        // Act
+        var mergedOptions = PasswordOptionsMerger.Merge(options);
+
+        // Assert
+        Assert.Equal(15, mergedOptions.Length);
+        Assert.True(mergedOptions.IncludeLowerCaseLetters);
+        Assert.True(mergedOptions.IncludeUpperCaseLetters);
+        Assert.True(mergedOptions.IncludeDigits);
+        Assert.True(mergedOptions.IncludeSymbols);
+    }
+
+    [Fact]
+    public void Should_Return_Options_With_Overridden_Length_And_Rest_Default_Setting_If_Only_Length_Is_Given()
+    {
+        // Arrange
+        var options = new PasswordOptions()
+        {
+            Length = 10
+        };
+
+        // Act
+        var mergedOptions = PasswordOptionsMerger.Merge(options);
+
+        // Assert
+        Assert.Equal(10, mergedOptions.Length);
+        Assert.True(mergedOptions.IncludeLowerCaseLetters);
+        Assert.True(mergedOptions.IncludeUpperCaseLetters);
+        Assert.True(mergedOptions.IncludeDigits);
+        Assert.True(mergedOptions.IncludeSymbols);
+    }
+
+    [Theory]
+    [InlineData(false, true, false, false)]
+    [InlineData(false, false, true, false)]
+    [InlineData(false, false, false, true)]
+    [InlineData(false, false, true, true)]
+    [InlineData(false, true, false, true)]
+    [InlineData(false, true, true, true)]
+    public void Should_Only_Allow_To_Set_IncludeLowerCase_False_If_At_Least_On_Of_Other_Char_Types_Are_True(
+        bool includeLowerCase,
+        bool includeUpperCase,
+        bool includeDigit,
+        bool includeSymbol)
+    {
+        // Arrange
+        var options = new PasswordOptions()
+        {
+            Length = 10,
+            IncludeLowerCaseLetters = includeLowerCase,
+            IncludeUpperCaseLetters = includeUpperCase,
+            IncludeDigits = includeDigit,
+            IncludeSymbols = includeSymbol
+        };
+
+        // Act
+        var mergedOptions = PasswordOptionsMerger.Merge(options);
+
+        // Assert
+        Assert.Equal(10, mergedOptions.Length);
+        Assert.False(mergedOptions.IncludeLowerCaseLetters);
+        Assert.Equal(includeUpperCase, mergedOptions.IncludeUpperCaseLetters);
+        Assert.Equal(includeDigit, mergedOptions.IncludeDigits);
+        Assert.Equal(includeSymbol, mergedOptions.IncludeSymbols);
+    }
+}
