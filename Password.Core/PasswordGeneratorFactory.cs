5503c60c48028f0f6d6ca65f31edd11d4973390d
Vedant Koditkar <vedant.koditkar@outlook.com>
2022-10-01 02:05:16 +0530

    Add PasswordGenerator factory

---
+++ Password.Core/PasswordGeneratorFactory.cs
@@ -0,0 +1,20 @@
+﻿namespace Password.Core;
+
+public static class PasswordGeneratorFactory
+{
+    public static IPasswordGenerator GetGenerator()
+    {
+        var systemSharedRandomNumberGenerator = new SystemSharedRandomNumberGenerator();
+        return new PasswordGenerator(
+            new EvenCharDistribution(),
+            new Dictionary<PasswordCharType, IRandomCharacterProvider>
+            {
+                { PasswordCharType.LowerCase, new RandomLowerCaseLetterProvider(systemSharedRandomNumberGenerator) },
+                { PasswordCharType.UpperCase, new RandomUpperCaseLetterProvider(systemSharedRandomNumberGenerator) },
+                { PasswordCharType.Digit, new RandomDigitProvider(systemSharedRandomNumberGenerator) },
+                { PasswordCharType.Symbol, new RandomSymbolProvider(systemSharedRandomNumberGenerator) }
+            },
+            systemSharedRandomNumberGenerator
+        );
+    }
+}
